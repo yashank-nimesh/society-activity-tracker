c2a1263046ed3355a321a4b3963f9a2481101f5d
yashank-nimesh <208054128+yashank-nimesh@users.noreply.github.com>
2026-08-28 12:13:30 +0530

    Add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </AuthProvider>
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>
 );
